49acece42a4e5ec54c2294a4b2845cb65e6b39f5
alisson-moura <alisson.mo.moura.outlook.com.br>
2020-01-15 22:38:35 -0300

    Update files

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//Componente: Bloco isolado de html, css e js, o qual não interfere no restante da aplicação
+//Propriedade: Informações que um componente PAI passa para o componente FILHO
+//Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello World</h1>
   );
 }
 
